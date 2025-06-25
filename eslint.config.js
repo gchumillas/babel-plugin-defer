27a05093f6232c9d7ba2212605c86232a816b93d
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-06-25 16:45:53 +0100

    Update ESLint parser options to use dynamic project path and root directory

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,8 @@ module.exports = [
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
-        project: './tsconfig.json'
+        project: true,
+        tsconfigRootDir: __dirname
       },
       globals: {
         console: 'readonly',
